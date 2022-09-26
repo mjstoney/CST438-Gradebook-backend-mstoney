d0d0defec9d69bdb7cd0bef8cef6e6b4be2e74ed
mjstoney <77265862+mjstoney@users.noreply.github.com>
2022-09-26 11:50:53 -0700

    Implement /course/{courseID}/finalgrades endpoint using both REST and rabbitMQ.

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceMQ.java
@@ -1,12 +1,15 @@
 package com.cst438.services;
 
 
+import java.util.List;
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.cst438.domain.AssignmentGrade;
 import com.cst438.domain.Course;
 import com.cst438.domain.CourseDTOG;
 import com.cst438.domain.CourseRepository;
@@ -43,9 +46,17 @@ public class RegistrationServiceMQ extends RegistrationService {
 	@RabbitListener(queues = "gradebook-queue")
 	@Transactional
 	public void receive(EnrollmentDTO enrollmentDTO) {
-		
+	
+		Course c = courseRepository.findByCourseID(enrollmentDTO.course_id);
+		List<AssignmentGrade> assignmentGrades = null;
 		//TODO  complete this method in homework 4
+		Enrollment e = new Enrollment();
+		e.setStudentEmail(enrollmentDTO.studentEmail);
+		e.setStudentName(enrollmentDTO.studentName);
+		e.setCourse(c);
+		e.setAssignmentGrades(assignmentGrades);
 		
+		enrollmentRepository.save(e);
 	}
 
 	// sender of messages to Registration Service
@@ -53,6 +64,8 @@ public class RegistrationServiceMQ extends RegistrationService {
 	public void sendFinalGrades(int course_id, CourseDTOG courseDTO) {
 		 
 		//TODO  complete this method in homework 4
+		rabbitTemplate.convertAndSend(registrationQueue.getName(), courseDTO);
+		System.out.println("Final grades sent to registration service.");
 		
 	}
 
